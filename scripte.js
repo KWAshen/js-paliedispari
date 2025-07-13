d33a9628600ebde5acab1012028bddb07eec5dad
KWAshen <ashen.maliaha@gmail.com>
2025-07-13 16:30:39 +0200

    funzione della parola

--- scripte.js
+++ scripte.js
@@ -1,1 +1,18 @@
 
+function palindroma(string){
+  const reversed = string.split("").reverse().join('').toLowerCase();
+  console.log(reversed);
+}
+
+
+
+const word = prompt('inserisci una parola');
+
+if(palindroma(word) === true){
+  console.log('è palindroma')
+}
+else{
+  console.log('non è palindroma');
+}
+
+
